ec97bc1e8fe9046d3cdb50e9a494d8ac4752b2a1
sebastianocalella <sebastianocalella97@gmail.com>
2022-12-02 17:36:34 +0100

    snack 1

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const buttonElement = document.getElementById('my-button');
 
 let conteggio = 0;
+let gameOver = false;
 
 buttonElement.addEventListener('click', function(){
-    if (conteggio < 10){
-        conteggio++;
-        buttonElement.innerHTML = "#" + conteggio;        
+    if(!gameOver){
+        if (conteggio < 10){
+            conteggio++;
+            buttonElement.innerHTML = "#" + conteggio;        
+        } else{
+            gameOver=true;
+            setTimeout(() => {
+                gameOver=false;
+                conteggio=0;
+                buttonElement.innerHTML = "#" + conteggio; 
+            },2000);
+        }
     }
-    setTimeout(exit,1500,conteggio);
 });
 
-function exit(counter){
-    counter = 0;
-}
