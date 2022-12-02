acf735bf18c9738e37ed1193b3f10092456382c3
sebastianocalella <sebastianocalella97@gmail.com>
2022-12-02 17:46:15 +0100

    upload files

---
+++ snack 2/script.js
@@ -0,0 +1,6 @@
+const tempo = document.getElementById('tempo');
+const start = document.getElementById('start');
+const pause = document.getElementById('pause');
+const reset = document.getElementById('reset');
+
+
