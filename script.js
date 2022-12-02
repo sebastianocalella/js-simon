04b6ebd3b47f66a120c80ab661f68f8ff7123b25
sebastianocalella <sebastianocalella97@gmail.com>
2022-12-02 17:25:04 +0100

    kind of

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const buttonElement = document.getElementById('my-button');
 let conteggio = 0;
 
 buttonElement.addEventListener('click', function(){
-    conteggio++;
-    if (conteggio >= 10){
-        setTimeout(exit,1500);
+    if (conteggio < 10){
+        conteggio++;
+        buttonElement.innerHTML = "#" + conteggio;        
     }
-    buttonElement.innerHTML = "#" + conteggio;
+    setTimeout(exit,1500,conteggio);
 });
+
+function exit(counter){
+    counter = 0;
+}
