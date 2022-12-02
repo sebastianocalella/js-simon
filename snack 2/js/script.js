ac0137fda15ceb322af98c11cecfb05b5be560ad
sebastianocalella <sebastianocalella97@gmail.com>
2022-12-02 17:59:32 +0100

    start system

--- snack 2/js/script.js
+++ snack 2/js/script.js
@@ -3,4 +3,12 @@ const start = document.getElementById('start');
 const pause = document.getElementById('pause');
 const reset = document.getElementById('reset');
 
-
+let i=0;
+start.addEventListener('click', function(){
+    if(i==0){
+        setInterval(function(){
+            i++;
+            tempo.innerHTML=i;
+        },1)
+    }
+});
