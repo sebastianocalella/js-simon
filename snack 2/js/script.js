6c8a3a5d7454c3817482ce242850d20050440203
sebastianocalella <sebastianocalella97@gmail.com>
2022-12-02 18:15:16 +0100

    exercise complete

--- snack 2/js/script.js
+++ snack 2/js/script.js
@@ -4,15 +4,22 @@ const pause = document.getElementById('pause');
 const reset = document.getElementById('reset');
 
 let i=0;
+tempo.innerHTML=i;
 start.addEventListener('click', function(){
     if(i==0 || value == i){
         let myInterval = setInterval(function(){
-                i++;
-                tempo.innerHTML=i;
+            i++;
+            tempo.innerHTML=i;
         },1)
         pause.addEventListener('click', function(){
             clearInterval(myInterval);
             value=i;
         });
+        reset.addEventListener('click',function(){
+            i=0;
+            tempo.innerHTML=i;
+            clearInterval(myInterval);
+        });
     }
 });
+
