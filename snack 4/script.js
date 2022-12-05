33798a92262752c5310c5ceb6e33e04538b29c76
Sebastiano Calella <sebastianocalella97@gmail.com>
2022-12-05 12:10:59 +0100

    total array

--- snack 4/script.js
+++ snack 4/script.js
@@ -1,17 +1,47 @@
-const auto ={
+const autoBenzina ={
     marca:'opel',
     modello:'astra',
-    alimentazione:'metano'
+    alimentazione:'benzina'
 }
 
-for(let key in auto){
-    console.log(auto[key]);
+for(let key in autoBenzina){
+    console.log(autoBenzina[key]);
 }
+
+const autoDiesel ={
+    marca:'opel',
+    modello:'astra',
+    alimentazione:'benzina'
+}
+
+for(let key in autoDiesel){
+    console.log(autoDiesel[key]);
+}
+
+const autoAltro ={
+    marca:'opel',
+    modello:'astra',
+    alimentazione:'altro'
+}
+
+for(let key in autoAltro){
+    console.log(autoAltro[key]);
+}
+
+
 
 const autos = [];
 
-for (let i =0; i<10; i++){
-    autos.push(auto);
+for (let i =0; i<4; i++){
+    autos.push(autoBenzina);
+}
+
+for (let i =0; i<3; i++){
+    autos.push(autoDiesel);
+}
+
+for (let i =0; i<3; i++){
+    autos.push(autoAltro);
 }
 
 
