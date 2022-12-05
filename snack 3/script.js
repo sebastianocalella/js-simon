f107c7e1af8b73a1d5c03c5e917ff255a7b1e78b
Sebastiano Calella <sebastianocalella97@gmail.com>
2022-12-05 11:43:33 +0100

    2 students in array

--- snack 3/script.js
+++ snack 3/script.js
@@ -1,9 +1,21 @@
-const student ={
+const studentOne ={
     name:'Adam',
     surname:'Adamander',
-    age:'22'
+    age:22
+}
+
+const studentTwo ={
+    name: 'Edgar',
+    surname: 'Edgardgander',
+    age: 33
 }
 
 for(let key in student){
     console.log(key + ": " + student[key]);
 }
+
+const classe = [];
+
+classe.push(studentOne,studentTwo);
+
+console.log(classe);
