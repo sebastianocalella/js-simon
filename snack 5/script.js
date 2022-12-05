cc1fe549150f59c70bfb38aca0a49d93c9639b7a
Sebastiano Calella <sebastianocalella97@gmail.com>
2022-12-05 12:27:32 +0100

    snack 5 elements

---
+++ snack 5/script.js
@@ -0,0 +1,52 @@
+const zucchines = [
+    {
+        varieta: 'romana',
+        lunghezza: 250,
+        peso: 800
+    },
+    {
+        varieta: 'romana',
+        lunghezza: 250,
+        peso: 400
+    },
+    {
+        varieta: 'romana',
+        lunghezza: 250,
+        peso: 500
+    },
+    {
+        varieta: 'romana',
+        lunghezza: 250,
+        peso: 300
+    },
+    {
+        varieta: 'romana',
+        lunghezza: 250,
+        peso: 700
+    },
+    {
+        varieta: 'romana',
+        lunghezza: 250,
+        peso: 800
+    },
+    {
+        varieta: 'romana',
+        lunghezza: 250,
+        peso: 800
+    },
+    {
+        varieta: 'romana',
+        lunghezza: 250,
+        peso: 400
+    },
+    {
+        varieta: 'romana',
+        lunghezza: 250,
+        peso: 200
+    },
+    {
+        varieta: 'romana',
+        lunghezza: 250,
+        peso: 500
+    }
+]
