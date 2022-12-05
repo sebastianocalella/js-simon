a32f0538e07ca15117aeed2773beb7b3e65cb610
Sebastiano Calella <sebastianocalella97@gmail.com>
2022-12-05 12:30:54 +0100

    snack 5

--- snack 5/script.js
+++ snack 5/script.js
@@ -49,4 +49,12 @@ const zucchines = [
         lunghezza: 250,
         peso: 500
     }
-]
+];
+
+let pesoTotale =0;
+for (let i=0; i<zucchines.length;i++){
+    const currentZucchina = zucchines[i];
+    pesoTotale+=currentZucchina.peso;
+}
+
+console.log(pesoTotale + " g");
